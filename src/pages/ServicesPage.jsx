784015f2d94622074bd85eea1b8e5e2c571d45cf
anthax589 <jaymartuba2@gmail.com>
2025-09-29 23:55:57 +0800

    feat: Revamp HeroSection layout and content for Recruitment Business service

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,7 +1,6 @@
 import Header from "../shared/Header";
 import HeroSection from "../components/Services/HeroSection";
 import OurServicesSection from "../components/Services/OurServicesSection";
-
 import Footer from "../shared/Footer";
 const LandingPage = () => {
   return (
