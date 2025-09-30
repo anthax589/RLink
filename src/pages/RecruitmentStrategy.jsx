a49583658acb403e51a42bfb266e322b6d2c9a2c
anthax589 <jaymartuba2@gmail.com>
2025-09-30 23:30:57 +0800

    feat: Add HrProducts and RecruitmentStrategy components, update pages to include new sections, and add new vector images

--- src/pages/RecruitmentStrategy.jsx
+++ src/pages/RecruitmentStrategy.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../shared/Header";
 import HeroSection from "../components/Services/RecruitmentStrategy/HeroSection";
+import Recruitment from "../components/Services/RecruitmentStrategy/RecruitmentStrategy";
 import Footer from "../shared/Footer";
 function RecruitmentStrategy() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Recruitment />
       <Footer />
     </div>
   );
